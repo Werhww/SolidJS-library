8c73f417d07b79a6fee2ae39f63cecc49059e25c
werhww <kleppeleo@gmail.com>
2022-12-06 22:17:31 +0100

    working button

--- labrary/src/components/buttons/buttons.tsx
+++ labrary/src/components/buttons/buttons.tsx
@@ -1,22 +1,78 @@
+import { classList } from 'solid-js/web'
 import style from './buttons.module.css'
 
+
 interface props {
+    // all buttons
     onclick:()=>void,
 
-    color?: string,
+    // all buttons with text
     children?:any,
+    color?: string,
+
+    // all buttos with icon
     icon?:any
+    
+    // only icon buttons
+    height?:string
+
+
+    witdh?:string
+
+
+    // only login buttons for now
+    fill_Color?:string
+    border_color?:string
+    text_color?:string
+    justify_content?:string
 }
 
-export default function button({children,onclick, color}:props) {
+export default function button({children,onclick, fill_Color, border_color, text_color, justify_content, witdh}:props) {
     return (
-    <div class={style.button} onclick={onclick}>
-        <p class={style.buttonText}>{children}</p>
-    </div>)
+    <p class={`${style.default} ${style.defaultButton}`} style={{
+        "border": `1px solid ${border_color}`,
+        "background-color": `1px solid ${border_color}`,
+        "justify-content": justify_content,
+        "width": witdh,
+        "color": `${text_color}`,
+    }} onclick={onclick}>
+        {children}
+    </p>)
 }
 
-export function IconButton({icon, onclick}:props){
-    return <div class={style.button} onclick={onclick}>
-        <img class={style.icon} src={icon} alt="icon" />
+export function SmallIconButton({icon, onclick}:props){
+    return <div class={`${style.default} ${style.iconShadow}`} onclick={onclick}>
+        <img class={style.smallIcon} src={icon}/>
+    </div>
+}
+
+export function MediumIconButton({icon, onclick}:props){
+    return <div class={`${style.default} ${style.iconShadow}`} onclick={onclick}>
+        <img class={style.mediumIcon} src={icon}/>
+    </div>
+}
+
+export function BigIconButton({icon, onclick}:props){
+    return <div class={`${style.default} ${style.iconShadow}`} onclick={onclick}>
+        <img class={style.bigIcon} src={icon}/>
+    </div>
+}
+
+export function CustomIconButton({icon, onclick, height}:props){
+    return <div class={`${style.default} ${style.iconShadow}`} onclick={onclick}>
+        <img src={icon} style={`height:${height};`}/>
+    </div>
+}
+
+export function LoginButton({children, icon, onclick, fill_Color, border_color, text_color, justify_content, witdh}:props){
+    return <div class={`${style.default} ${style.loginButton}`} 
+    style={`
+        border: 1px solid ${border_color}; 
+        background-color: ${fill_Color}; 
+        justify-content: ${justify_content}; 
+        width: ${witdh};`
+    } onClick={onclick}>
+        <img class={style.loginIcon} src={icon} />
+        <p style={`color: ${text_color};`}>{children}</p>
     </div>
 }
--- labrary/src/pages/workingComponents/workingComponents.tsx
+++ labrary/src/pages/workingComponents/workingComponents.tsx
@@ -1,19 +1,28 @@
 import style from './workingComponents.module.css'
 
 import Button from '../../components/buttons/buttons'
-import { IconButton } from '../../components/buttons/buttons'
+import { LoginButton } from '../../components/buttons/buttons'
 
+import { SmallIconButton } from '../../components/buttons/buttons'
+import { MediumIconButton } from '../../components/buttons/buttons'
+import { BigIconButton } from '../../components/buttons/buttons'
+import { CustomIconButton } from '../../components/buttons/buttons'
 
 // working
-import icon from '../../assets/example-logo.svg'
+import logo from '../../assets/example-logo.svg'
+import googleIcon from '../../assets/example-loginIcon.svg'
 
 export default function module(){
-    function exsample() {
+    function example() {
         console.log('test')
     }
 
-    return <div>
-        <IconButton onclick={exsample} icon={icon}></IconButton>
-        <Button onclick={exsample}>test</Button>
+    return <div class={style.body}>
+        <SmallIconButton onclick={example} icon={googleIcon}></SmallIconButton>
+        <MediumIconButton onclick={example} icon={googleIcon}></MediumIconButton>
+        <BigIconButton onclick={example} icon={googleIcon}></BigIconButton>
+        <CustomIconButton onclick={example} icon={googleIcon} height={'10rem'}> </CustomIconButton>
+        <Button onclick={example}>test</Button>
+        <LoginButton onclick={example} icon={googleIcon} justify_content="center">Sign in with Google</LoginButton>
     </div>
 }
