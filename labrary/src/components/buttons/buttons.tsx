212db8ec4d7f323fb6c2090e72281cd77548bc84
werhww <kleppeleo@gmail.com>
2022-12-08 18:16:33 +0100

    working button

--- labrary/src/components/buttons/buttons.tsx
+++ labrary/src/components/buttons/buttons.tsx
@@ -1,137 +1,98 @@
-import style from './buttons.module.css'
-var root = document.querySelector(':root');
+import styles from './buttons.module.css'
+var root:any = document.querySelector(':root');
 
 interface props {
-    // all buttons
-    onclick:()=>void,
-    witdh?:string
-
-    // all buttons with text
-    children?:any,
-    color?: string,
-    fill_Color?:string,
+    background_Color?:string,
     border_color?:string,
+    border_radius?:string,
+    drop_shadow_color?:string,
     text_color?:string,
-    justify_content?:string
-
-    // all buttos with icon
-    icon?:any
-
-    // preset Icons
-    arrow?:string,
-    add?:string,
-    share?:string,
-    plus?:string,
-    dowload?:string,
-    upload?:string,
-    
-    // only icon buttons
+    font_size?:string,
+    align_items?:string,
+    justify_content?:string,
     height?:string,
-    filter?:string
+    width?:string,
+
+    customClass?:string
+
+    onclick?:()=>void,
+
+    children?:any,
+    icon?:any,
+    filter?:string,
+    rotate?:string,
 }
 
-export default function button({children,onclick, fill_Color, border_color, text_color, justify_content, witdh}:props) {
+interface defaults{
+    background_Color?:string,
+    border_color?:string,
+    border_radius?:string,
+    drop_shadow_color?:string,
+    text_color?:string,
+    font_size?:string,
+    height?:string,
+    width?:string,
+    align_items?:string,
+    justify_content?:string,
+}
+
+export function DefaultButtonStyles({background_Color, border_color, border_radius, drop_shadow_color, text_color, font_size, height, width, align_items, justify_content}:defaults){
+    const properties = {
+        "--button_background_color": background_Color,
+        '--button_border_color': border_color,
+        '--button_border_radius': border_radius,
+        '--button_drop_shadow_color': drop_shadow_color,
+        '--button_text_color': text_color,
+        '--button_font_size': font_size,
+        '--button_height': height,
+        '--button_width': width,
+        '--button_align_items': align_items,
+        '--button_justify_content': justify_content
+    }
+
+    for(var [key,value] of Object.entries(properties)){
+        if(value)root.style.setProperty(key, value);
+    }
+    return <></>
+}
+
+export default function Button({onclick, children, customClass}:props) { 
     return (
-    <p class={`${style.default} ${style.defaultButton}`} style={{
-        "border": `1px solid ${border_color}`,
-        "background-color": `1px solid ${border_color}`,
-        "justify-content": justify_content,
-        "width": witdh,
-        "color": `${text_color}`,
-    }} onclick={onclick}>
+    <p class={`${styles.default} ${customClass}`} onclick={onclick}>
         {children}
     </p>)
 }
 
-export function CustomIconButton({icon, onclick, height}:props){
-    return <div class={`${style.default} ${style.iconShadow}`} onclick={onclick}>
-        <img src={icon} style={`height:${height};`}/>
+
+export function IconButton({onclick, icon, rotate, filter, height, width}:props){
+    return <div class={styles.iconButton} onclick={onclick}>
+        <img src={icon} 
+            style={{
+                "height": height,
+                "width": width,
+                "rotate": rotate,
+                "filter": filter
+            }}
+        />
     </div>
 }
 
-/// Icon svg imports ///
-import arrow1 from './icons/arrow-filled.svg'
-import arrow2 from './icons/arrow-outline.svg'
-import arrow3 from './icons/circle-arrow-filled.svg'
-import arrow4 from './icons/circle-arrow-outline.svg'
-
-import add1 from './icons/add-user-filled.svg'
-import add2 from './icons/add-user-outline.svg'
-import add3 from './icons/new-file-1.svg'
-import add4 from './icons/new-file-2.svg'
-import add5 from './icons/new-folder-1.svg'
-
-import share1 from './icons/share-arrows-filled.svg'
-import share2 from './icons/share-arrows-outline.svg'
-
-import plus1 from './icons/plus-sign-filled.svg'
-import plus2 from './icons/plus-sign-outline.svg'
-import plus3 from './icons/plus-square-outline.svg'
-
-import upload2 from './icons/upload.svg'
-import download1 from './icons/download.svg'
-
-export function IconButton({onclick, arrow, add, share, plus, dowload, upload, height, filter}:props){
-    function icon(){
-        if(arrow == "1"){
-            return arrow1
-        } else if(arrow == "2"){
-            return arrow2
-        } else if(arrow == "3"){
-            return arrow3
-        } else if(arrow == "4"){
-            return arrow4
-        }
-
-        if(add == "1"){
-            return add1
-        } else if(add == "2"){
-            return add2
-        } else if(add == "3"){
-            return add3
-        } else if(add == "4"){
-            return add4
-        } else if(add == "5"){
-            return add5
-        }
-
-        if(share == "1"){
-            return share1
-        } else if(share == "2"){
-            return share2
-        }
-
-        if(plus == "1"){
-            return plus1
-        } else if(plus == "2"){
-            return plus2
-        } else if(plus == "3"){
-            return plus3
-        }
-
-        if(dowload == "1"){
-            return download1
-        } else if(upload == "1"){
-            return upload2
-        }
-
-    }
-    return <img class={`${style.default} ${style.iconButton}`} src={icon()} style={{
-        "height": height,
-        "filter": filter,
-    }} onClick={onclick}/>
-}
-
-export function TextIconButton({children, icon, onclick, fill_Color, border_color, text_color, justify_content, witdh, filter}:props){
-    return <div class={`${style.default} ${style.textIconButton}`} 
-    style={{
-        "border": `1px solid ${border_color}`, 
-        "background-color":fill_Color, 
+export function TextIconButton({children, icon, onclick, filter, background_Color, border_color, border_radius, text_color, justify_content, width, height}:props){
+    return <div class={styles.textIconButton} onClick={onclick}
+    style={{ 
+        "background-color":background_Color,
+        "border": `1px solid ${border_color}`,
+        "border-radius": border_radius,
+        "color": text_color,
         "justify-content": justify_content, 
-        "width": witdh, 
-        "filter": filter
-        }} onClick={onclick}>
-        <img class={style.loginIcon} src={icon} />
+        "width": width,
+        }}>
+        <img class={styles.loginIcon} src={icon} 
+            style={{
+                "filter": filter, 
+                "height": height
+            }}
+        />
         <p style={`color: ${text_color};`}>{children}</p>
     </div>
 }
--- labrary/src/pages/workingComponents/workingComponents.tsx
+++ labrary/src/pages/workingComponents/workingComponents.tsx
@@ -1,9 +1,9 @@
 import style from './workingComponents.module.css'
 
 import Button from '../../components/buttons/buttons'
+import { DefaultButtonStyles } from '../../components/buttons/buttons'
 import { TextIconButton } from '../../components/buttons/buttons'
 import { IconButton } from '../../components/buttons/buttons'
-import { CustomIconButton } from '../../components/buttons/buttons'
 
 // working
 import logo from '../../assets/example-logo.svg'
@@ -14,10 +14,19 @@ export default function module(){
         console.log('test')
     }
 
+
     return <div class={style.body}>
-        <CustomIconButton onclick={example} icon={googleIcon}> </CustomIconButton>
-        <Button onclick={example}>test</Button>
-        <TextIconButton onclick={example} icon={googleIcon}>Sign in with Google</TextIconButton>
-        <IconButton onclick={example} arrow="1"></IconButton>
+        <DefaultButtonStyles 
+            background_Color='var(--blue)' 
+            border_color='var(--blue)' 
+            drop_shadow_color='var(blue)'
+            border_radius='5rem'
+            text_color='white' 
+            width='5rem'
+        ></DefaultButtonStyles>
+
+        <Button onclick={example}>Save</Button>
+        <TextIconButton onclick={example} icon={logo} width="20rem" height='13rem'>Log in with google</TextIconButton>
+        <IconButton onclick={example} icon={googleIcon} height='3rem'></IconButton>
     </div>
 }
