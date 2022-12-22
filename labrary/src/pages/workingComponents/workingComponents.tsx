6f6906e63c2b15fdaaa803f284f2ccef5a69e922
werhww <kleppeleo@gmail.com>
2022-12-22 23:55:52 +0100

    fix input background to transparent

--- labrary/src/pages/workingComponents/workingComponents.tsx
+++ labrary/src/pages/workingComponents/workingComponents.tsx
@@ -1,8 +1,7 @@
 import style from './workingComponents.module.css'
 
-import Navbar from '../../components/navbar/navbar'
-import Navlink from '../../components/navbar/navlink/navlink'
-import DropLink from '../../components/navbar/dropLink/dropLink'
+import Input from '../../components/inputs/inputs'
+
 
 import logo from "../../assets/example-logo.svg"
 
@@ -13,13 +12,12 @@ export default function module(){
 
     function change() {
         console.log('hello')
-        console.log(selectObject.value)
     }
 
     return <div class={style.body}>
-        <Navbar logo={logo} logoLink="/all">
-            <Navlink active={false} link='/test'>Home</Navlink>
-            <Navlink active={true} link='/'>Back</Navlink>
-        </Navbar>
+        <div class={style.inputs}>
+            <Input valueCb={change}>test</Input>
+        </div>
+        
     </div>
 }
